ada5e9b79acd94e633940f32c875e55b195ae617
Manfred <marodok@gmail.com>
2020-01-22 12:52:36 -0600

    Updates for padma 1.2.0

--- padma-content-slider.php
+++ padma-content-slider.php
@@ -4,7 +4,7 @@
 Plugin Name:	Padma Content Slider
 Plugin URI:		https://www.padmaunlimited/plugins/content-slider
 Description:  	Padma Content Slider is a WordPress plugin that turns your posts into a post slider in different styles. Post slider is touch friendly and responsive for all devices.
-Version:		1.0.4
+Version:		1.0.6
 Author: 		Padma Unlimited Team
 Author URI: 	https://www.padmaunlimited.com/
 License:      	GPL2
@@ -43,7 +43,30 @@ function register_content_slider_block() {
 	if (!class_exists('PadmaBlockAPI') )
 		return false;
 
+
+	$class = 'PadmaContentSliderBlock';
+	$block_type_url = substr(WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), '', plugin_basename(__FILE__)), 0, -1);		
+	$class_file = __DIR__ . '/block.php';
+	$icons = array(
+			'path' => __DIR__,
+			'url' => $block_type_url
+		);
+
+	padma_register_block(
+		$class,
+		$block_type_url,
+		$class_file,
+		$icons
+	);
 	
-	return padma_register_block('PadmaContentSliderBlock', substr(WP_PLUGIN_URL . '/' . str_replace(basename(__FILE__), '', plugin_basename(__FILE__)), 0, -1));
+
+	/**
+	 *
+	 * Check if there is the Padma Loader
+	 *
+	 */		
+	if ( version_compare(PADMA_VERSION, '1.1.70', '<=') ){			
+		include_once $class_file;
+	}
 
 }
